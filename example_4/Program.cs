d6f391be0d2865215b1e02ea95379f108f6e002d
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-03 15:19:29 -0400

    example_4

---
+++ example_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Напишите три числа, и мы покажем вам наибольшее.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if(number2 > max)
+{
+    Console.WriteLine("Наибольшее число: " + number2);
+}
+else if(number3 > max)
+{
+    Console.WriteLine("Наибольшее число: " + number3);
+}
+else
+{
+    Console.WriteLine("Наибольшее число: " + max);
+}
