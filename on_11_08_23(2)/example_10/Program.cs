4e07df8c643a164907aa6b3a15801d8d4f547758
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-08-05 18:05:55 -0400

    add_example_13_in_on_11_08_2

--- on_11_08_23(2)/example_10/Program.cs
+++ on_11_08_23(2)/example_10/Program.cs
@@ -1,6 +1,11 @@
 ﻿Console.Write("Введите трехзначное число, и мы покажем вам его десятки: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if(number < 0)
+{
+    number = number * -1;
+}
+
 if(number > 999 || number < 100)
 {
     Console.WriteLine("Введите трёхзначное число!");
