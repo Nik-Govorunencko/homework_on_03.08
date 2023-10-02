6ffb5743e4eb22ab8d2c35b9f9da6f2a4aa91dbf
nik.govorunencko <nik.govorunencko@yandex.ru>
2023-10-01 22:51:56 -0400

    homework_on28.09.23

---
+++ 00.09.23/on_28.09.23/js/app.js
@@ -0,0 +1,12 @@
+alert("Введите своё имя в поле!");
+function send() {
+    let massage = document.getElementById('massage').value;
+    document.getElementById('result').innerText = `Привет ${massage}!!!`;
+}
+
+
+
+
+
+
+
